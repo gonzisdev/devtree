8540aa653371cb541855563a5a388eff252faeb0
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-12-06 17:38:49 +0100

    added react hook form, error message component, register form and types

---
+++ frontend/src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react"
+
+const ErrorMessage = ({children}: PropsWithChildren) => {
+  return (
+    <p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold">{children}</p>
+  )
+}
+
+export default ErrorMessage
