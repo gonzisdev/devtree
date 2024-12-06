c294e98b8317c984dffd192cdae07021335d2698
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-12-06 10:45:49 +0100

    added login and middleware

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,36 +1,47 @@
 import { Request, Response } from "express"
 import slug from "slug"
 import User from "../models/User"
-import { hashPassword } from "../utils/auth"
+import { checkPassword, hashPassword } from "../utils/auth"
 import { validationResult } from "express-validator"
 
-
 export const createAccount = async (req: Request, res: Response) => {
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() })
-        return
-    }
+  const { email, password } = req.body
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    const error = new Error("Un usuario con ese mail ya esta registrado")
+    res.status(409).json({ error: error.message })
+    return
+  }
+
+  const handle = slug(req.body.handle, "")
+  const handleExists = await User.findOne({ handle })
+  if (handleExists) {
+    const error = new Error("Nombre de usuario no disponible")
+    res.status(409).json({ error: error.message })
+    return
+  }
+
+  const user = new User(req.body)
+  user.password = await hashPassword(password)
+  user.handle = handle
+
+  await user.save()
+  res.status(201).send("Registro creado correctamente")
+}
+
+export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body
-    const userExists = await User.findOne({ email })
-    if (userExists) {
-        const error = new Error('Un usuario con ese mail ya esta registrado')
-        res.status(409).json({ error: error.message })
-        return
+    const user = await User.findOne({ email })
+    if (!user) {
+      const error = new Error("El usuario no existe")
+      res.status(404).json({ error: error.message })
+      return
     }
 
-    const handle = slug(req.body.handle, '')
-    const handleExists = await User.findOne({ handle })
-    if (handleExists) {
-        const error = new Error('Nombre de usuario no disponible')
-        res.status(409).json({ error: error.message })
-        return
+    const isPasswordCorrect = await checkPassword(password, user.password)
+    if (!isPasswordCorrect) {
+      const error = new Error("La contraseña no es correcta")
+      res.status(401).json({ error: error.message })
+      return 
     }
-
-    const user = new User(req.body)
-    user.password = await hashPassword(password)
-    user.handle = handle
-
-    await user.save()
-    res.status(201).send('Registro Creado Correctamente')
 }
