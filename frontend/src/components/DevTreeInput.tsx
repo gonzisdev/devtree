5ca5873bf06446bddf767ade79c9d7958a12e992
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-12-29 13:16:10 +0100

    added input and switch handle functions

--- frontend/src/components/DevTreeInput.tsx
+++ frontend/src/components/DevTreeInput.tsx
@@ -1,23 +1,36 @@
 import { Switch } from '@headlessui/react'
-import type { DevTreeLink } from '../types'
+import type { DevTreeLink, SocialNetwork } from '../types'
 import { classNames } from '../utils'
+import { ChangeEvent } from 'react'
 
 type DevTreeInputProps = {
   item: DevTreeLink
+  handleUrlChange: (e: ChangeEvent<HTMLInputElement>) => void
+  handleEnabledLink: (socialNetwork: SocialNetwork['name']) => void
 }
 
-const DevTreeInput = ({ item }: DevTreeInputProps) => {
+const DevTreeInput = ({
+  item,
+  handleUrlChange,
+  handleEnabledLink,
+}: DevTreeInputProps) => {
   return (
     <div className='bg-white shadow-sm p-5 flex items-center gap-3'>
       <div
         className='w-12 h-12 bg-cover'
         style={{ backgroundImage: `url(/social/icon_${item.name}.svg)` }}
       ></div>
-      <input type='text' className='flex-1 border border-gray-100 rounded-lg' />
+      <input
+        type='text'
+        className='flex-1 border border-gray-100 rounded-lg'
+        value={item.url}
+        onChange={handleUrlChange}
+        name={item.name}
+      />
 
       <Switch
         checked={item.enabled}
-        onChange={() => {}}
+        onChange={() => handleEnabledLink(item.name)}
         className={classNames(
           item.enabled ? 'bg-blue-500' : 'bg-gray-200',
           'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { social } from '../data/social'
 import DevTreeInput from '../components/DevTreeInput'
+import type { SocialNetwork } from '../types'
 
 const LinkTreeView = () => {
   const [devTreeLinks, setDevTreeLinks] = useState(social)
+
+  const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    )
+    setDevTreeLinks(updatedLinks)
+  }
+
+  const handleEnabledLink = (socialNetwork: SocialNetwork['name']) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === socialNetwork ? { ...link, enabled: !link.enabled } : link
+    )
+    setDevTreeLinks(updatedLinks)
+  }
 
   return (
     <>
       <div className='space-y-5'>
         {devTreeLinks.map((item) => (
-          <DevTreeInput key={item.id} item={item} />
+          <DevTreeInput
+            key={item.name}
+            item={item}
+            handleUrlChange={handleUrlChange}
+            handleEnabledLink={handleEnabledLink}
+          />
         ))}
       </div>
     </>
